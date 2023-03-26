dae22e48c7035624bbe42ca3ed9d18b5e8e658f2
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-03-26 18:22:26 +0430

    add new-blog url structure for add new blog from client

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import UserLogin,UserLogout,UserRegister
+from .views import UserLogin,UserLogout,UserRegister,NewBlog
 
 
 app_name:str = "user"
@@ -9,4 +9,5 @@ urlpatterns = [
     path("register",UserRegister.as_view(),name="userregister"),
     path("login",UserLogin.as_view(),name="userlogin"),
     path("logout",UserLogout.as_view(),name="logout"),
+    path("new-blog",NewBlog.as_view(),name="newblog"),
 ]
