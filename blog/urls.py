f30d1e3f979cba4498491feec745a8d998894a4c
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-03-26 15:39:20 +0430

    add LastBlog service url path

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import AllBlogs,LikeBlog,ViewBlog,SingleBlog,SearchBlog,BlogWithCategory,CommentBlog
+from .views import AllBlogs,LikeBlog,ViewBlog,SingleBlog,SearchBlog,BlogWithCategory,CommentBlog,LastBlogs
 
 
 app_name:str = "blog"
 
 urlpatterns = [
+    path("",LastBlogs.as_view(),name="lastblogs"),
     path("blogs",AllBlogs.as_view(),name="allblogs"),
     path("like-blog/<int:blog_id>",LikeBlog.as_view(),name="likeblog"),
     path("view-blog/<int:blog_id>",ViewBlog.as_view(),name="viewblog"),
