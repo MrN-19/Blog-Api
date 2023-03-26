3c2fb9e08c84574ab5ba416f50e186218ce1dcca
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-03-26 18:23:01 +0430

    add NewBlog service for add blogs from clients

--- user/views.py
+++ user/views.py
@@ -3,8 +3,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser
 
 from .serializers import UserRegisterSerializer,UserLoginSerializer
+from blog.serializers import BlogSerializerFromUser
 
 from django.contrib.auth import login,logout
 
@@ -40,3 +42,24 @@ class UserLogout(APIView):
 
         Token.objects.filter(user = request.user).delete()
         return Response(status=status.HTTP_200_OK)
+    
+
+class NewBlog(APIView):
+
+    permission_classes = (IsAuthenticated,)
+    parser_classes = (MultiPartParser,)
+
+    def post(self,request):
+
+        blog_serializer_from_user = BlogSerializerFromUser(data = request.data,user = request.user)
+
+        if blog_serializer_from_user.is_valid():
+            blog_serializer_from_user.save()
+
+            data = {
+                "message" : "Your Blog Added Successfully ..."
+            }
+
+            return Response(data,status=status.HTTP_200_OK)
+        
+        return Response(data = blog_serializer_from_user.error_messages,status=status.HTTP_400_BAD_REQUEST)
