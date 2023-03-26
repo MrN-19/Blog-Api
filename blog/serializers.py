0e388708550350706da883cb501017050fee9b8d
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-03-26 18:20:57 +0430

    add BlogSerializerFromUser class and add override create method of this class

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,6 +1,10 @@
+from typing import Dict,Any
+
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
-from .models import Blog, BlogComment
+from .models import Blog, BlogComment,BlogCategory
 
 
 class CommentsSerializer(serializers.ModelSerializer):
@@ -47,3 +51,64 @@ class CommentSerializer(serializers.Serializer):
             user = self.__user
         )
     
+class BlogSerializerFromUser(serializers.Serializer):
+
+    def __init__(self, user,instance=None, data=..., **kwargs):
+        super().__init__(instance, data, **kwargs)
+        self.__user = user
+
+    @property
+    def user(self) -> User:
+        return self.__user
+
+    category_id = serializers.IntegerField(default=0)
+
+    title = serializers.CharField(max_length = 150,error_messages = {
+        "required" : "Please Enter Title",
+        "max_length" : "Entred Title is too long"
+    })
+
+    short_describtion = serializers.CharField(max_length = 500,error_messages = {
+        "required" : "Please Enter Short Describtion",
+        "max_length" : "Entred Short Describtion is too long"
+    })
+
+    picture = serializers.ImageField(error_messages = {
+        "blank" : "Please Enter Picture"
+    })
+
+    text = serializers.CharField(error_messages = {
+        "required" : "Please Enter Text !"
+    })
+    
+    def create(self, validated_data:Dict[str,Any]):
+
+        category_id:int = validated_data.get("category_id",0)
+
+        if category_id == 0:
+            raise serializers.ValidationError("Category Id is not Valid ... !")
+        if not self.__user:
+            raise serializers.ValidationError("User Is Not Valid ... !")
+
+        category = BlogCategory.objects.filter(id = category_id).first()
+
+        if not category:
+            raise serializers.ValidationError("Category Not Found ... !")
+
+        title:str = validated_data.get("title","")
+        short_describtion:str = validated_data.get("short_describtion","")
+        text:str = validated_data.get("text","")
+
+        picture = validated_data.get("picture",None)
+
+        new_blog = Blog(
+            user = self.__user,
+            category = category,
+            title = title,
+            short_description = short_describtion,
+            text = text,
+            picture = picture
+        )
+        
+        return new_blog
+        
